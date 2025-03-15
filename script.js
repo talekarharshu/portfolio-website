fe7349aa645988687e29abbdd4049e421c253d06
talekarharshu <149597781+talekarharshu@users.noreply.github.com>
2025-03-16 00:23:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+function Function1() {
+    var text=document.getElementById("demo1");
+    text.innerHTML="Intermediate Level Developer of HTML"
+}
+function Function2() {
+    var text=document.getElementById("demo2");
+    text.innerHTML="Intermediate Level Developer of CSS"
+}
+function Function3() {
+    var text=document.getElementById("demo3");
+    text.innerHTML=" Beginer Level Developer of Javascript"
+}
+function Function4() {
+    var text=document.getElementById("demo4");
+    text.innerHTML="Beginer Level User"
+}
+function Function5() {
+    var text=document.getElementById("demo5");
+    text.innerHTML="Beginer Level Developer of React"
+}
+function Function6() {
+    var text=document.getElementById("demo6");
+    text.innerHTML="Intermediate Level Developer Of Python"
+}
+function Function7() {
+    var text=document.getElementById("demo7");
+    text.innerHTML="Intermediate Level User"
+}
+function Function8() {
+    var text=document.getElementById("demo8");
+    text.innerHTML="Beginer Level Developer of Java "
+}
